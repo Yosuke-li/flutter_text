a8a4afdc61d6cac5ad3a442011e6bc6dc4a715d6
coco <690575679@qq.com>
2019-11-25 17:25:56 +0800

    11-25 v01 1.U 优化上拉评论页面

--- lib/assembly_pack/sliding_up_panel.dart
+++ lib/assembly_pack/sliding_up_panel.dart
@@ -10,12 +10,63 @@ class SlidingUpText extends StatelessWidget {
       appBar: AppBar(
         title: Text('SlidingUpPanel组件'),
       ),
-      body: SlidingUpPanel(
-        panel: Center(
-          child: Text('抽屉区'),
+      body: SlidingDemo(),
+    );
+  }
+}
+
+class SlidingDemo extends StatefulWidget {
+  SlidingDemoState createState() => SlidingDemoState();
+}
+
+class SlidingDemoState extends State<SlidingDemo> {
+  PanelController panel = new PanelController();
+  double offsetDistance = 0.0;
+  double offsetY=0;
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: SlidingUpPanel(
+        controller: panel,
+        minHeight: 0,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(24.0),
+          topRight: Radius.circular(24.0),
         ),
-        body: Center(
-          child: Text('页面区'),
+        panel: Container(
+          child: Center(
+            child: Text("评论区",style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.normal,decoration:TextDecoration.none)),
+          ),
+        ),
+        body: GestureDetector(
+          child: new ConstrainedBox(
+            constraints: BoxConstraints.expand(),
+            child: new Image.asset(
+              "images/lake.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          onTap: (){
+            panel.close();
+          },
+          onVerticalDragDown: (details){
+            // print(details.globalPosition.dy);
+            offsetDistance=details.globalPosition.dy;
+          },
+          onVerticalDragUpdate: (details){
+            offsetY=details.globalPosition.dy-offsetDistance;
+            if(offsetY>0){
+              print("向下"+offsetY.toString());
+            }else{
+              print("向上"+offsetY.toString());
+              double position=offsetY.abs()/300;
+              position=position>1?1:position;
+              panel.setPanelPosition(position);
+              if(position>0.4){
+                panel.open();
+              }
+            }
+          },
         ),
       ),
     );
