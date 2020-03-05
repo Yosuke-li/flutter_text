70c7713fffd2f20e99afc401a1da6c1e19912d7d
coco <690575679@qq.com>
2020-03-05 18:30:01 +0800

    1.U chat聊天系统

--- lib/assembly_pack/chat/chat_main.dart
+++ lib/assembly_pack/chat/chat_main.dart
@@ -6,17 +6,54 @@ class chatPackApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "聊天",
-      home: chatScene(),
+      home: ChatScene(),
     );
   }
 }
 
-class chatScene extends StatelessWidget {
+class ChatScene extends StatefulWidget {
+  @override
+  ChatSceneState createState() => ChatSceneState();
+}
+
+class ChatSceneState extends State<ChatScene> {
+  final TextEditingController _textEditingController =
+      new TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textEditingController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 6.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _textEditingController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: "发送消息"),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textEditingController.text),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("聊天"),
       ),
+      body: _buildTextComposer(),
     );
   }
 }
