acae396379fa6b19d1c1d7fd4883f683bfca43bb
coco <690575679@qq.com>
2020-03-06 17:32:56 +0800

    1.U 谷歌登录实现

--- lib/assembly_pack/chat/chat_main.dart
+++ lib/assembly_pack/chat/chat_main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_text/assembly_pack/chat/sign_in.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(chatPackApp());
 
@@ -16,12 +18,48 @@ class ChatScene extends StatefulWidget {
   ChatSceneState createState() => ChatSceneState();
 }
 
+GoogleSignIn googleSignIn = new GoogleSignIn(
+  scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
+
 class ChatSceneState extends State<ChatScene> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textEditingController =
       new TextEditingController();
+  bool isComposer;
+  GoogleSignInAccount _currentUser;
 
-  void _handleSubmitted(String text) {
+  //google登录
+  Future<void> _ensureLoggedIn() async {
+    googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      setState(() {
+        _currentUser = account;
+      });
+    });
+    if (_currentUser == null) await googleSignIn.signInSilently();
+    if (_currentUser == null) await googleSignIn.signIn();
+  }
+
+  Future<void> _handleSubmitted(String text) async {
     _textEditingController.clear();
+    setState(() {
+      isComposer = false;
+    });
+    await _ensureLoggedIn();
+    _sendMessage(text: text);
+  }
+
+  //发送消息
+  void _sendMessage({String text}) {
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 
   Widget _buildTextComposer() {
@@ -33,6 +71,11 @@ class ChatSceneState extends State<ChatScene> {
             child: TextField(
               controller: _textEditingController,
               onSubmitted: _handleSubmitted,
+              onChanged: (String text) {
+                setState(() {
+                  isComposer = text.length > 0;
+                });
+              },
               decoration: InputDecoration.collapsed(hintText: "发送消息"),
             ),
           ),
@@ -40,7 +83,9 @@ class ChatSceneState extends State<ChatScene> {
             margin: EdgeInsets.symmetric(horizontal: 4.0),
             child: IconButton(
               icon: Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textEditingController.text),
+              onPressed: () => isComposer
+                  ? _handleSubmitted(_textEditingController.text)
+                  : null,
             ),
           ),
         ],
@@ -52,8 +97,65 @@ class ChatSceneState extends State<ChatScene> {
     return Scaffold(
       appBar: AppBar(
         title: Text("聊天"),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => SignInDemo()),
+            ),
+            child: Text("SignIn"),
+          )
+        ],
       ),
-      body: _buildTextComposer(),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemCount: _messages.length,
+                itemBuilder: (_, int index) {
+                  return _messages[index];
+                }),
+          ),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.only(right: 16),
+              child: GoogleUserCircleAvatar(
+                identity: googleSignIn.currentUser,
+              )),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(googleSignIn.currentUser.displayName),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
