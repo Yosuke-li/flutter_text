4a2e9fd912b48e01b905d7276384da2e6a2dea72
coco <690575679@qq.com>
2020-03-11 14:27:21 +0800

    1.U 发送消息成功 2.U 连接firebase实时数据库成功 3.U 发送图片并显示成功

--- lib/assembly_pack/chat/chat_main.dart
+++ lib/assembly_pack/chat/chat_main.dart
@@ -1,3 +1,8 @@
+import 'dart:math';
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_text/assembly_pack/chat/sign_in.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -5,13 +10,14 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() => runApp(chatPackApp());
 
 class chatPackApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "聊天",
+      title: "聊天室",
       home: ChatScene(),
     );
   }
@@ -23,7 +29,7 @@ class ChatScene extends StatefulWidget {
 }
 
 //google登录
-DatabaseReference reference = FirebaseDatabase.instance.reference().child('messages');
+DatabaseReference reference;
 GoogleSignIn googleSignIn = new GoogleSignIn(
   scopes: [
     'email',
@@ -34,7 +40,6 @@ GoogleSignIn googleSignIn = new GoogleSignIn(
 class ChatSceneState extends State<ChatScene> {
   final auth = FirebaseAuth.instance; //监听用户UID
   final analytics = new FirebaseAnalytics(); //监听事件
-  final List<ChatMessage> _messages = <ChatMessage>[]; //监听信息
   final TextEditingController _textEditingController =
       new TextEditingController(); //输入框
   bool isComposer;
@@ -43,10 +48,24 @@ class ChatSceneState extends State<ChatScene> {
   //生命周期
   void initState() {
     super.initState();
-    _ensureLoggedIn();
+    _setDatabase();
   }
 
-  //google登录
+  //firebase database设置
+  Future<void> _setDatabase() async {
+    final App = await FirebaseApp.configure(
+      name: 'db2',
+      options: FirebaseOptions(
+        googleAppID: '1:32984109309:android:30927d8d01aa5b244eae94',
+        apiKey: 'AIzaSyDmY0jjaHswJ65DP9KI4oEA4iyCkXiCZxQ',
+        databaseURL: 'https://flutter-talk-app-3ed74.firebaseio.com',
+      ),
+    );
+    var db = FirebaseDatabase(app: App);
+    reference = db.reference().child('messages');
+  }
+
+  //google登录 配置信息
   Future<void> _ensureLoggedIn() async {
     googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
       setState(() {
@@ -71,34 +90,56 @@ class ChatSceneState extends State<ChatScene> {
     }
   }
 
+  //删除聊天框里的消息并发送
   Future<void> _handleSubmitted(String text) async {
     _textEditingController.clear();
     setState(() {
       isComposer = false;
     });
+    await _ensureLoggedIn();
     _sendMessage(text: text);
   }
 
   //发送消息
-  void _sendMessage({String text}) {
-    ChatMessage message = new ChatMessage(
-      text: text,
-    );
-    setState(() {
-      _messages.insert(0, message);
+  void _sendMessage({String text, String imageUrl}) {
+    reference.push().set({
+      'text': text,
+      'imageUrl': imageUrl,
+      'senderName': googleSignIn.currentUser.displayName,
+      'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
     });
     analytics.logEvent(name: 'send_message'); //google事件日志
   }
 
+  //选择图片
+  void _pickerImage() async {
+    File imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    int random = new Random().nextInt(100000);
+    StorageReference ref =
+        FirebaseStorage.instance.ref().child("image_$random.jpg");
+    StorageUploadTask uploadTask = ref.putFile(imageFile);
+    await uploadTask.onComplete;
+    String downloadUrl = await uploadTask.lastSnapshot.ref.getDownloadURL();
+    _sendMessage(imageUrl: downloadUrl);
+  }
+
+  //底部组件
   Widget _buildTextComposer() {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 6.0),
       child: Row(
         children: <Widget>[
+          Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+                icon: new Icon(Icons.photo_camera),
+                onPressed: () {
+                  _pickerImage();
+                }),
+          ),
           Flexible(
             child: TextField(
               controller: _textEditingController,
-              onSubmitted: _handleSubmitted,
               onChanged: (String text) {
                 setState(() {
                   isComposer = text.length > 0;
@@ -121,30 +162,26 @@ class ChatSceneState extends State<ChatScene> {
     );
   }
 
+  //聊天消息查看
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("聊天"),
-        actions: <Widget>[
-          GestureDetector(
-            onTap: () => Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => SignInDemo()),
-            ),
-            child: Text("SignIn"),
-          )
-        ],
+        actions: <Widget>[],
       ),
       body: Column(
         children: <Widget>[
           Flexible(
-              child: ListView.builder(
-                  padding: EdgeInsets.all(8.0),
+              child: FirebaseAnimatedList(
+                  query: reference,
+                  sort: (a, b) => b.key.compareTo(a.key),
+                  padding: new EdgeInsets.all(8.0),
                   reverse: true,
-                  itemCount: _messages.length,
-                  itemBuilder: (_, int index) {
-                    return _messages[index];
-                  }),
-          ),
+                  itemBuilder: (_, DataSnapshot snapshot,
+                      Animation<double> animation, __) {
+                    return ChatMessage(
+                        snapshot: snapshot, animation: animation);
+                  })),
           Divider(height: 1.0),
           Container(
             decoration: BoxDecoration(color: Theme.of(context).cardColor),
@@ -156,14 +193,17 @@ class ChatSceneState extends State<ChatScene> {
   }
 }
 
+//消息
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
+  ChatMessage({this.snapshot, this.animation});
 
-  final String text;
+  final DataSnapshot snapshot;
+  final Animation animation;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizeTransition(
+      sizeFactor: new CurvedAnimation(parent: animation, curve: Curves.easeOut),
       child: Container(
         margin: EdgeInsets.symmetric(vertical: 10.0),
         child: Row(
@@ -171,16 +211,22 @@ class ChatMessage extends StatelessWidget {
           children: <Widget>[
             Container(
                 margin: EdgeInsets.only(right: 16),
-                child: GoogleUserCircleAvatar(
-                  identity: googleSignIn.currentUser,
-                )),
+                child: CircleAvatar(
+                    child: Image.network(snapshot.value['senderPhotoUrl']))),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(googleSignIn.currentUser.displayName),
+                Text(snapshot.value['senderName']),
                 Container(
                   margin: EdgeInsets.only(top: 5.0),
-                  child: Text(text),
+                  child: snapshot.value['imageUrl'] != null
+                      ? new Image.network(
+                          snapshot.value['imageUrl'],
+                          width: 250.0,
+                          height: 100.0,
+                          fit: BoxFit.fill,
+                        )
+                      : new Text(snapshot.value['text']),
                 ),
               ],
             )
