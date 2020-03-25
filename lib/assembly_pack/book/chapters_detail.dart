1288e6dcf0d352cfaaa814f6dd385410afb6ee3c
coco <690575679@qq.com>
2020-03-25 09:00:23 +0800

    1.U 微调代码

--- lib/assembly_pack/book/chapters_detail.dart
+++ lib/assembly_pack/book/chapters_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_text/api/book.dart';
 import 'package:flutter_text/model/book.dart';
 
@@ -17,6 +18,7 @@ class ChaptersDetail extends StatefulWidget {
 class ChaptersDetailState extends State<ChaptersDetail> {
   ChapterInfo _chapterInfo;
   bool isShow = false;
+  bool isShowBar = true;
 
   void getChaptersDetail() async {
     final result = await BookApi().getChaptersDetail(widget.link);
@@ -31,16 +33,23 @@ class ChaptersDetailState extends State<ChaptersDetail> {
 
   void initState() {
     super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([]);
     getChaptersDetail();
+  }
+
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIOverlays(
+        [SystemUiOverlay.top, SystemUiOverlay.bottom]);
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: isShow
-          ? SingleChildScrollView(
-              child: Stack(
-                children: <Widget>[
-                  Container(
+          ? Stack(
+              children: <Widget>[
+                SingleChildScrollView(
+                  child: Container(
                     padding:
                         EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
                     child: Text(
@@ -49,20 +58,46 @@ class ChaptersDetailState extends State<ChaptersDetail> {
                       strutStyle: StrutStyle(leading: 1.2),
                     ),
                   ),
-                  Positioned(
-                      top: MediaQuery.of(context).size.height * 0.2,
-                      left: MediaQuery.of(context).size.width * 0.2,
-                      child: Container(
-                        height: MediaQuery.of(context).size.height * 0.6,
-                        width: MediaQuery.of(context).size.width * 0.6,
-                        child: GestureDetector(
-                          onTap: () {
-                            print('11111');
+                ),
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  child: Container(
+                    color: Color(0x40000000),
+                    height: MediaQuery.of(context).size.height * 0.08,
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
                           },
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
                         ),
-                      ))
-                ],
-              ),
+                        Text(
+                          _chapterInfo.title,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                    top: MediaQuery.of(context).size.height * 0.2,
+                    left: MediaQuery.of(context).size.width * 0.2,
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: GestureDetector(
+                        onTap: () {
+                          print('11111');
+                        },
+                      ),
+                    ))
+              ],
             )
           : Center(
               child: CircularProgressIndicator(),
