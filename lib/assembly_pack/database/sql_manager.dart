ee4660b06573ea4b4aa4a4329f675840f1708925
coco <690575679@qq.com>
2020-09-24 17:41:35 +0800

    U 1.sql

---
+++ lib/assembly_pack/database/sql_manager.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_text/utils/date_format.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class SqlManager {
+  static const _VERSION = 1;
+
+  static const _NAME = 'my.db';
+
+  static Database _database;
+
+  //初始化
+  static Future<void> init() async {
+    final dataBasePath = await getDatabasesPath();
+    String path = join(dataBasePath, _NAME);
+    _database = await openDatabase(path,
+        version: _VERSION, onCreate: (Database db, int version) async {});
+  }
+
+  static Future<bool> isTableExits(String tableName) async {
+    await getCurrentDatabase();
+    final result = await _database.rawQuery(
+        'select * from Sql_text where type= "table" and name = "$tableName"');
+    return result != null && result.length > 0;
+  }
+
+  //获取当前数据库
+  static Future<Database> getCurrentDatabase() async {
+    if (_database == null) {
+      await init();
+    }
+    return _database;
+  }
+
+  static close() {
+    _database.close();
+    _database = null;
+  }
+}
