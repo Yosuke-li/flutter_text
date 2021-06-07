1167c5eaedb91ca39c1944401d2078121a8c52b8
Nayo <690575679@qq.com>
2021-06-07 09:23:49 +0800

    fix: 打包失败

---
+++ lib/assembly_pack/dll_text/dll_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ffi/ffi.dart' as ffi;
+import 'package:call/call.dart';
+
+class DllTextPage extends StatefulWidget {
+
+  @override
+  _DllTextPageState createState() => _DllTextPageState();
+}
+
+class _DllTextPageState extends State<DllTextPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
