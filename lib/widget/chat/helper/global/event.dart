43c8fe864fea622c73cd75a855e5c8f67ebea066
李铉 <coco@fospot.net>
2021-07-02 14:49:02 +0800

    style: db准备

--- lib/widget/chat/helper/global/event.dart
+++ lib/widget/chat/helper/global/event.dart
@@ -3,3 +3,10 @@ import 'package:mqtt_client/mqtt_client.dart';
 class EventChat {
   MqttReceivedMessage msg;
 }
+
+class DbGlobal {
+  static String ip = '47.102.41.197';
+  static int port = 5433;
+  static String database = 'db4d3c49473a7e4472961204b7a3838a5euser';
+  static String username = 'nayo';
+}
---
+++ lib/widget/chat/helper/user/user_db.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_text/widget/chat/helper/global/event.dart';
+import 'package:postgres/postgres.dart';
+
+class PostgresUser {
+  // static connection = PostgreSQLConnection(DbGlobal.ip, DbGlobal.port, DbGlobal.database, username: )
+}
