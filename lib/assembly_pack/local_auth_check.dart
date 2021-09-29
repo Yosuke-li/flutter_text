85b1499476c3d946f175dcdaf7000915078782c1
Nayo <690575679@qq.com>
2021-09-29 16:09:58 +0800

    style: 生物验证

--- lib/assembly_pack/local_auth_check.dart
+++ lib/assembly_pack/local_auth_check.dart
@@ -9,29 +9,37 @@ class LocalAuthCheck extends StatefulWidget {
   _localAuthCheckState createState() => _localAuthCheckState();
 }
 
+enum _SupportState {
+  unknown,
+  supported,
+  unsupported,
+}
+
 class _localAuthCheckState extends State<LocalAuthCheck> {
-  /// 本地认证框架
   final LocalAuthentication auth = LocalAuthentication();
-
-  /// 是否有可用的生物识别技术
+  _SupportState _supportState = _SupportState.unknown;
   bool _canCheckBiometrics;
-
-  /// 生物识别技术列表
   List<BiometricType> _availableBiometrics;
-
-  /// 识别结果
-  String _authorized = '验证失败';
-
+  String _authorized = 'Not Authorized';
   bool _isAuthenticating = false;
 
-  /// 检查是否有可用的生物识别技术
+  @override
+  void initState() {
+    super.initState();
+    auth.isDeviceSupported().then(
+          (isSupported) => setState(() => _supportState = isSupported
+          ? _SupportState.supported
+          : _SupportState.unsupported),
+    );
+  }
+
   Future<void> _checkBiometrics() async {
     bool canCheckBiometrics;
     try {
       canCheckBiometrics = await auth.canCheckBiometrics;
     } on PlatformException catch (e) {
+      canCheckBiometrics = false;
       print(e);
-      rethrow;
     }
     if (!mounted) return;
 
@@ -40,12 +48,12 @@ class _localAuthCheckState extends State<LocalAuthCheck> {
     });
   }
 
-  /// 获取生物识别技术列表
   Future<void> _getAvailableBiometrics() async {
     List<BiometricType> availableBiometrics;
     try {
       availableBiometrics = await auth.getAvailableBiometrics();
     } on PlatformException catch (e) {
+      availableBiometrics = <BiometricType>[];
       print(e);
     }
     if (!mounted) return;
@@ -55,7 +63,6 @@ class _localAuthCheckState extends State<LocalAuthCheck> {
     });
   }
 
-  /// 生物识别
   Future<void> _authenticate() async {
     bool authenticated = false;
     try {
@@ -63,56 +70,140 @@ class _localAuthCheckState extends State<LocalAuthCheck> {
         _isAuthenticating = true;
         _authorized = 'Authenticating';
       });
-      authenticated = await auth.authenticateWithBiometrics(
-          localizedReason: '扫描指纹进行身份验证',
+      authenticated = await auth.authenticate(
+          localizedReason: 'Let OS determine authentication method',
           useErrorDialogs: true,
-          stickyAuth: false);
+          stickyAuth: true);
+      setState(() {
+        _isAuthenticating = false;
+      });
+    } on PlatformException catch (e) {
+      print(e);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = "Error - ${e.message}";
+      });
+      return;
+    }
+    if (!mounted) return;
+
+    setState(
+            () => _authorized = authenticated ? 'Authorized' : 'Not Authorized');
+  }
+
+  Future<void> _authenticateWithBiometrics() async {
+    bool authenticated = false;
+    try {
+      setState(() {
+        _isAuthenticating = true;
+        _authorized = 'Authenticating';
+      });
+      authenticated = await auth.authenticate(
+          localizedReason:
+          'Scan your fingerprint (or face or whatever) to authenticate',
+          useErrorDialogs: true,
+          stickyAuth: true,
+          biometricOnly: true);
       setState(() {
         _isAuthenticating = false;
         _authorized = 'Authenticating';
       });
     } on PlatformException catch (e) {
       print(e);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = "Error - ${e.message}";
+      });
+      return;
     }
     if (!mounted) return;
 
+    final String message = authenticated ? 'Authorized' : 'Not Authorized';
     setState(() {
-      _authorized = authenticated ? '验证通过' : '验证失败';
+      _authorized = message;
     });
   }
 
-  void _cancelAuthentication() {
-    auth.stopAuthentication();
+  void _cancelAuthentication() async {
+    await auth.stopAuthentication();
+    setState(() => _isAuthenticating = false);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('插件的示例应用程序'),
-      ),
-      body: ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Text('是否有可用的生物识别技术: $_canCheckBiometrics\n'),
-                RaisedButton(
-                  child: const Text('检查生物识别技术'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.only(top: 30),
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (_supportState == _SupportState.unknown)
+                  CircularProgressIndicator()
+                else if (_supportState == _SupportState.supported)
+                  Text("This device is supported")
+                else
+                  Text("This device is not supported"),
+                Divider(height: 100),
+                Text('Can check biometrics: $_canCheckBiometrics\n'),
+                ElevatedButton(
+                  child: const Text('Check biometrics'),
                   onPressed: _checkBiometrics,
                 ),
-                Text('可用的生物识别技术: $_availableBiometrics\n'),
-                RaisedButton(
-                  child: const Text('获取可用的生物识别技术'),
+                Divider(height: 100),
+                Text('Available biometrics: $_availableBiometrics\n'),
+                ElevatedButton(
+                  child: const Text('Get available biometrics'),
                   onPressed: _getAvailableBiometrics,
                 ),
-                Text('状态: $_authorized\n'),
-                RaisedButton(
-                  child: const Text('验证'),
-                  onPressed: _authenticate,
+                Divider(height: 100),
+                Text('Current State: $_authorized\n'),
+                (_isAuthenticating)
+                    ? ElevatedButton(
+                  onPressed: _cancelAuthentication,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("Cancel Authentication"),
+                      Icon(Icons.cancel),
+                    ],
+                  ),
                 )
-              ])),
-    ));
+                    : Column(
+                  children: [
+                    ElevatedButton(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text('Authenticate'),
+                          Icon(Icons.perm_device_information),
+                        ],
+                      ),
+                      onPressed: _authenticate,
+                    ),
+                    ElevatedButton(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(_isAuthenticating
+                              ? 'Cancel'
+                              : 'Authenticate: biometrics only'),
+                          Icon(Icons.fingerprint),
+                        ],
+                      ),
+                      onPressed: _authenticateWithBiometrics,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
