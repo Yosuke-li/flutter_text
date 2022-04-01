41d6599c30695a69bb4d07693b9e80b662e38122
Nayo <690575679@qq.com>
2022-04-01 10:35:10 +0800

    style: 鼠标右键

--- lib/assembly_pack/desktop_list/desktop_picker.dart
+++ lib/assembly_pack/desktop_list/desktop_picker.dart
@@ -1,4 +1,5 @@
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter_text/init.dart';
 
 class DesktopPickerPage extends StatefulWidget {
@@ -8,9 +9,7 @@ class DesktopPickerPage extends StatefulWidget {
   _DesktopPickerState createState() => _DesktopPickerState();
 }
 
-class _DesktopPickerState extends State<DesktopPickerPage>  {
-
-  
+class _DesktopPickerState extends State<DesktopPickerPage> {
   @override
   void initState() {
     super.initState();
@@ -43,12 +42,32 @@ class _DesktopPickerState extends State<DesktopPickerPage>  {
     Log.info(result);
   }
 
+  // 鼠标右键
+  Future<void> _onPointerDown(PointerDownEvent event) async {
+    List<PopupMenuItem<int>> menuItems = <PopupMenuItem<int>>[
+      PopupMenuItem(child: Text('apply +1'), value: 1),
+      PopupMenuItem(child: Text('apply -1'), value: 2),
+      PopupMenuItem(child: Text('set to 0'), value: 3),
+    ];
+
+
+    if (event.kind == PointerDeviceKind.mouse &&
+        event.buttons == kSecondaryMouseButton) {
+      Log.info(event);
+      final overlay =
+      Overlay.of(context).context.findRenderObject() as RenderBox;
+      final menuItem = await showMenu<int>(
+          context: context,
+          items: menuItems,
+          position: RelativeRect.fromSize(
+              event.position & Size(48.0, 48.0), overlay.size));
+    }
+  }
+
   //选择custom类型需要带上相应的后缀
   void _pickerCustom() async {
-    final FilePickerResult result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ['epub']
-    );
+    final FilePickerResult result = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: ['epub']);
     Log.info(result);
   }
 
@@ -70,6 +89,20 @@ class _DesktopPickerState extends State<DesktopPickerPage>  {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Listener(
+                      onPointerDown: (PointerDownEvent event) {
+                        _onPointerDown(event);
+                      },
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Log.info('111');
+                        },
+                        child: const Text('text'),
+                      ),
+                    ),
                     const SizedBox(
                       height: 20,
                     ),
