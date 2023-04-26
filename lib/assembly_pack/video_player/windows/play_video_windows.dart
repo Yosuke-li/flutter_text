cc4ec2eeb9cc55efeec621a2e175617c20adcc42
李铉 <coco@fospot.cn>
2023-04-26 10:43:13 +0800

    style: 拖拽本地视频播放

--- lib/assembly_pack/video_player/windows/play_video_windows.dart
+++ lib/assembly_pack/video_player/windows/play_video_windows.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:cross_file/cross_file.dart';
+import 'package:desktop_drop/desktop_drop.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:self_utils/utils/lock.dart';
 
@@ -21,6 +23,7 @@ class _PlayVideoWindowsState extends State<PlayVideoWindows> {
   double _width = 240;
 
   final Lock _lock = Lock();
+  XFile? _file;
 
   @override
   void initState() {
@@ -62,174 +65,104 @@ class _PlayVideoWindowsState extends State<PlayVideoWindows> {
     }
   }
 
+  // 拖拽本地视频播放
+  void _dragDone(DropDoneDetails details) async {
+    _file = details.files.last;
+    if (_file != null) {
+      final File res = await WinVideoHelper.setLocalVideoPath(
+          file: File(_file!.path),
+          name: _file!.name);
+
+      final WinVideoModel videoModel = WinVideoModel()
+        ..name = _file!.name
+        ..id = _file!.name.hashCode
+        ..path = res.path;
+
+      videoList.add(videoModel);
+      current = videoModel;
+      setState(() {});
+      WinVideoCache.setLocalVideos(videoModel);
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: RepaintBoundary(
-                        child: VideoPlayWinPage(
-                          key: current == null
-                              ? null
-                              : Key(current.hashCode.toString()),
-                          title: current?.name ?? '',
-                          file: current == null ? null : File(current!.path!),
-                          onExtraFunc: () {
-                            if (_width != 0) {
-                              _width = 0;
-                            } else {
-                              _width = 240;
-                            }
-                            setState(() {});
-                          },
-                        ),
-                      ),
-                    ),
-                    AnimatedContainer(
-                      duration: const Duration(milliseconds: 500),
-                      height: _height,
-                      child: SingleChildScrollView(
-                        child: Container(
-                          child: Row(
-                            children: [
-                              Container(
-                                color: Colors.amberAccent,
-                                width: 160,
-                                height: 200,
-                                child: InkWell(
-                                    onTap: () {
-                                      _height = 0;
-                                      setState(() {});
-                                    },
-                                    child: Text('封面')),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Container(
-                                          child: const Text('标题:'),
-                                        ),
-                                        Container(
-                                          child: Text(''),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Container(
-                                          child: const Text('简介:'),
-                                        ),
-                                        Container(
-                                          child: Text(''),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              width: _width,
-              child: Container(
-                color: Colors.white,
+      body: DropTarget(
+        onDragDone: _dragDone,
+        child: Container(
+          child: Row(
+            children: [
+              Expanded(
                 child: Container(
                   child: Column(
                     children: [
                       Expanded(
-                        child: SingleChildScrollView(
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: videoList.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              final WinVideoModel res =
-                                  ArrayHelper.get(videoList, index)!;
-                              return InkWell(
-                                onTap: () {
-                                  if (current != res) {
-                                    current = res;
-                                    setState(() {});
-                                  }
-                                },
-                                child: Container(
-                                  height: 40,
-                                  padding: const EdgeInsets.only(
-                                      left: 20, right: 15),
-                                  color: current == res
-                                      ? Colors.lightBlueAccent
-                                      : null,
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                        child: Text(
-                                          res.name ?? '',
-                                          style: TextStyle(
-                                              color: current == res
-                                                  ? Colors.white
-                                                  : null),
-                                        ),
-                                      ),
-                                      IconButton(
-                                        onPressed: () {
-                                          videoList.remove(res);
-                                          WinVideoCache.delete(res);
-                                          setState(() {});
-                                        },
-                                        icon: Icon(Icons.delete,
-                                            size: 18,
-                                            color: current == res
-                                                ? Colors.white
-                                                : null),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
+                        child: RepaintBoundary(
+                          child: VideoPlayWinPage(
+                            key: current == null
+                                ? null
+                                : Key(current.hashCode.toString()),
+                            title: current?.name ?? '',
+                            file: current == null ? null : File(current!.path!),
+                            onExtraFunc: () {
+                              if (_width != 0) {
+                                _width = 0;
+                              } else {
+                                _width = 240;
+                              }
+                              setState(() {});
                             },
                           ),
                         ),
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          if (!_lock.isUsing()) {
-                            _pickerVideo();
-                          }
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.only(
-                              right: 30, left: 30, bottom: 10),
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.add,
-                                color: Colors.white,
-                              ),
-                              Text(
-                                '添加视频',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ],
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 500),
+                        height: _height,
+                        child: SingleChildScrollView(
+                          child: Container(
+                            child: Row(
+                              children: [
+                                Container(
+                                  color: Colors.amberAccent,
+                                  width: 160,
+                                  height: 200,
+                                  child: InkWell(
+                                      onTap: () {
+                                        _height = 0;
+                                        setState(() {});
+                                      },
+                                      child: Text('封面')),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            child: const Text('标题:'),
+                                          ),
+                                          Container(
+                                            child: Text(''),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Container(
+                                            child: const Text('简介:'),
+                                          ),
+                                          Container(
+                                            child: Text(''),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -237,8 +170,102 @@ class _PlayVideoWindowsState extends State<PlayVideoWindows> {
                   ),
                 ),
               ),
-            ),
-          ],
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                width: _width,
+                child: Container(
+                  color: Colors.white,
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: videoList.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                final WinVideoModel res =
+                                    ArrayHelper.get(videoList, index)!;
+                                return InkWell(
+                                  onTap: () {
+                                    if (current != res) {
+                                      current = res;
+                                      setState(() {});
+                                    }
+                                  },
+                                  child: Container(
+                                    height: 40,
+                                    padding: const EdgeInsets.only(
+                                        left: 20, right: 15),
+                                    color: current == res
+                                        ? Colors.lightBlueAccent
+                                        : null,
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Text(
+                                            res.name ?? '',
+                                            style: TextStyle(
+                                                color: current == res
+                                                    ? Colors.white
+                                                    : null),
+                                          ),
+                                        ),
+                                        IconButton(
+                                          onPressed: () {
+                                            videoList.remove(res);
+                                            WinVideoCache.delete(res);
+                                            setState(() {});
+                                          },
+                                          icon: Icon(Icons.delete,
+                                              size: 18,
+                                              color: current == res
+                                                  ? Colors.white
+                                                  : null),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            if (!_lock.isUsing()) {
+                              _pickerVideo();
+                            }
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(
+                                right: 30, left: 30, bottom: 10),
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Row(
+                              children: const [
+                                Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  '添加视频',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
